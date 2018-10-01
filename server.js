db6937c75640f1efa34f0fe5aae6bf0b0207b7b1
Shubham Goyal <Shubham.Goyal@qlik.com>
2018-10-01 12:34:33 +0530

    update

--- server.js
+++ server.js
@@ -2,8 +2,7 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var http = require('http');
-//var logger = require('morgan');
+var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -15,7 +14,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-//app.use(logger('dev'));
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -33,7 +32,7 @@ app.use(function(req, res, next) {
 });
 
 // error handler
-/*app.use(function(err, req, res, next) {
+app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -41,10 +40,5 @@ app.use(function(req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
-});*/
-var server_app = http.createServer(app);
-
-// Start listening to the port
-server_app.listen(process.env.PORT || 5000, function () {
-  console.log('listening on http://localhost:' + (process.env.PORT || 5000)); //8080
 });
+module.exports = app;
